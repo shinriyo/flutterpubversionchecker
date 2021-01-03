f9e6c98db05204e089eeb3b40499ec7fd1af21d7
shinriyo <shinriyo@gmail.com>
2021-01-03 18:15:28 +0900

    fix name

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
+ * Copyright (C) shinriyo. All rights reserved.
  *--------------------------------------------------------*/
 
 import * as vscode from 'vscode';
